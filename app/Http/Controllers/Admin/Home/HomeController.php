d373a022f27ad63713b143ba75a534fe5b5ac946
unknown <heyreza8@gmail.com>
2024-11-17 18:42:40 +0330

    add admin home controller and admin home route

---
+++ app/Http/Controllers/Admin/Home/HomeController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Admin\Home;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(Request $request)
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
+use App\Http\Controllers\Admin\Home\HomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::prefix('admin')->group(function () {
+    Route::get('/', HomeController::class)->name('admin.index');
 });
