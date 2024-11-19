755acd5d40f76dba755332e26e9d2ed0408e58b4
unknown <heyreza8@gmail.com>
2024-11-19 22:12:57 +0330

    finish post page and fix some bug on the login

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -18,6 +18,10 @@ class LoginController extends Controller
             return back();
         }
 
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
         return to_route('admin.index');
     }
 }
--- resources/views/app/post.blade.php
+++ resources/views/app/post.blade.php
@@ -3,12 +3,16 @@
 @section('content')
 <section class="row">
     <section class="col-md-12">
-        <h1>title</h1>
+        <h1>{{ $post->title }}</h1>
         <h5 class="d-flex justify-content-between align-items-center">
-            <a href="">name</a>
-            <span class="date-time">22/22/33</span>
+            <a href="{{ route('app.category',$post->category->id) }}">{{ $post->category->name }}</a>
+            <span class="date-time">{{ $post->created_at->format('Y/m/d') }}</span>
         </h5>
-        <article class="bg-article p-3"><img class="float-right mb-2 ml-2" style="width: 18rem;" src="" alt="">body</article>
+        <article class="bg-article p-3">
+                <img class="w-100 mb-2 ml-2" style="width: 18rem;" src="{{ asset($post->image['indexArray']['medium']) }}" alt="">
+
+            {{ $post->body }}
+        </article>
 
 
     </section>
