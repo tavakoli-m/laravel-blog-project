88801498de504f63a53036ef8c0685c4ad791394
unknown <heyreza8@gmail.com>
2024-11-19 18:13:54 +0330

    finish register user controller and views

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -3,14 +3,23 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\RegisterRequest;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class RegisterController extends Controller
 {
     public function registerView(){
         return view('auth.register');
     }
-    public function register(){
-        dd("register");
+    public function register(RegisterRequest $request){
+        $userData = $request->validated();
+
+        $userData['password'] = Hash::make($userData['password']);
+
+        $user = User::create($userData);
+
+        return to_route('app.login');
     }
 }
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,30 +1,23 @@
 @extends('auth.layouts.master')
 
 @section('content')
-<form class="pt-3 pb-1 px-2 bg-light rounded-bottom" action="" method="post">
-                    <section class="form-group">
-                        <label for="email">Email</label>
-                        <input type="email" class="form-control" name="email" id="email" placeholder="email ...">
-                    </section>
-                    <section class="form-group">
-                        <label for="first_name">First Name</label>
-                        <input type="text" class="form-control" name="first_name" id="first_name" placeholder="first_name ...">
-                    </section>
-                    <section class="form-group">
-                        <label for="last_name">Last Name</label>
-                        <input type="text" class="form-control" name="last_name" id="last_name" placeholder="last_name ...">
-                    </section>
-                    <section class="form-group">
-                        <label for="password">Password</label>
-                        <input type="password" class="form-control" name="password" id="password" placeholder="password ...">
-                    </section>
-                    <section class="form-group">
-                        <label for="confirm">Confirm</label>
-                        <input type="password" class="form-control" name="confirm" id="confirm" placeholder="confirm ...">
-                    </section>
-                    <section class="mt-4 mb-2 d-flex justify-content-between">
-                        <input type="submit" class="btn btn-success btn-sm" value="register">
-                        <a class="" href="">login</a>
-                    </section>
-                </form>
+<form class="pt-3 pb-1 px-2 bg-light rounded-bottom" action="{{ route('app.register') }}" method="POST">
+    @csrf
+    <section class="form-group">
+        <label for="email">Email</label>
+        <input type="email" class="form-control" name="email" id="email" placeholder="email ...">
+    </section>
+    <section class="form-group">
+        <label for="name">Name</label>
+        <input type="text" class="form-control" name="name" id="name" placeholder="name ...">
+    </section>
+    <section class="form-group">
+        <label for="password">Password</label>
+        <input type="password" class="form-control" name="password" id="password" placeholder="password ...">
+    </section>
+    <section class="mt-4 mb-2 d-flex justify-content-between">
+        <input type="submit" class="btn btn-success btn-sm" value="register">
+        <a class="" href="{{ route('app.login') }}">login</a>
+    </section>
+</form>
 @endsection
