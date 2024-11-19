ce499f124834d737ac96d251f017fb2e6e12370e
unknown <heyreza8@gmail.com>
2024-11-19 22:06:27 +0330

    finish category page in app

--- resources/views/app/category.blade.php
+++ resources/views/app/category.blade.php
@@ -3,18 +3,21 @@
 @section('content')
 <section class="row">
     <section class="col-12">
-        <h1>name</h1>
+        <h1>{{ $category->name }}</h1>
         <hr>
     </section>
 </section>
 <section class="row">
 
-    <section class="col-md-4">
-        <section class="mb-2 overflow-hidden" style="max-height: 15rem;"><img class="img-fluid" src="" alt=""></section>
-        <h2 class="h5 text-truncate">title</h2>
-        <p>body</p>
-        <p><a class="btn btn-primary" href="" role="button">View details »</a></p>
-    </section>
+@foreach ($category->posts as $post)
 
+
+    <section class="col-md-4">
+        <section class="mb-2 overflow-hidden" style="max-height: 15rem;"><img class="img-fluid" src="{{ asset($post->image['indexArray']['medium']) }}" alt=""></section>
+        <h2 class="h5 text-truncate">{{ $post->title }}</h2>
+        <p>{{ $post->body }}</p>
+        <p><a class="btn btn-primary" href="{{ route('app.post',$post) }}" role="button">View details »</a></p>
+    </section>
+    @endforeach
 </section>
 @endsection
