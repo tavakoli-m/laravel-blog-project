e48ffc98fe529bf1d697d643d7be552327de9fb1
unknown <heyreza8@gmail.com>
2024-11-24 16:41:56 +0330

    add form request service

---
+++ app/Http/Services/FormRequest/FormRequest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Services\FormRequest;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest as BaseFormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class FormRequest extends BaseFormRequest
+{
+
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'errors' => $validator->errors()
+        ], 422));
+    }
+
+    protected function failedAuthorization()
+    {
+        throw new HttpResponseException(response()->json([
+            'message' => 'Access denied'
+        ], 403));
+    }
+
+}
