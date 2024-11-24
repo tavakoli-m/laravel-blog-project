131dbac2d50a23d047638a5109b0f2485a00a84b
unknown <heyreza8@gmail.com>
2024-11-24 17:46:24 +0330

    add api auth request validations

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -3,10 +3,12 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Api\Auth\LoginRequest;
+use App\Http\Requests\Api\Auth\RegisterRequest;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    public function register() {}
-    public function login() {}
+    public function register(RegisterRequest $request) {}
+    public function login(LoginRequest $request) {}
 }
