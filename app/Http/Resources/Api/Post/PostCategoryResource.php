4bd9afe8a78e5aa0ae5567b52ad47738a19cc9df
unknown <heyreza8@gmail.com>
2024-11-24 16:55:54 +0330

    coding api resource and and post category resource

--- app/Http/Resources/Api/Post/PostCategoryResource.php
+++ app/Http/Resources/Api/Post/PostCategoryResource.php
@@ -5,7 +5,7 @@ namespace App\Http\Resources\Api\Post;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class PostResource extends JsonResource
+class PostCategoryResource extends JsonResource
 {
     /**
      * Transform the resource into an array.
@@ -15,12 +15,7 @@ class PostResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
-            'id' => $this->id,
-            'title' => $this->title,
-            'body' => $this->body,
-            'category' => new PostCategoryResource($this->category),
-            'image' => $this->image,
-            'image_base_url' => asset('/')
+            'name' => $this->name,
         ];
     }
 }
