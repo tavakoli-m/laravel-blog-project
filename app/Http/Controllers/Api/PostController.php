b592b85869a0fc034552444f03c0314aaa61d343
unknown <heyreza8@gmail.com>
2024-11-24 17:32:56 +0330

    cooding post controller and add routes

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -3,6 +3,13 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Api\Post\StorePostRequest;
+use App\Http\Requests\Api\Post\UpdatePostRequest;
+use App\Http\Resources\Api\Post\PostResource;
+use App\Http\Resources\Api\Post\PostResourceCollection;
+use App\Http\Services\Image\ImageService;
+use App\Models\Post;
+use FastResponse\FastResponse\Facades\Response;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
@@ -12,38 +19,81 @@ class PostController extends Controller
      */
     public function index()
     {
-        //
+        $posts = Post::all();
+        return Response::withStatus(200)->withAppends([new PostResourceCollection($posts)])->send();
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StorePostRequest $request, ImageService $imageService)
     {
-        //
+        $inputs = $request->validated();
+
+        $imageService->setExclusiveDirectory('images' . DIRECTORY_SEPARATOR . 'posts');
+        $result = $imageService->createIndexAndSave($request->file('image'));
+        if ($result === false) {
+            return back();
+        }
+        $inputs['image'] = $result;
+
+        $post = Post::create($inputs);
+
+        return Response::withStatus(200)->withAppends([new PostResource($post)])->send();
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Post $post)
     {
-        //
+        return Response::withStatus(200)->withAppends([new PostResource($post)])->send();
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(UpdatePostRequest $request, post $post, ImageService $imageService)
     {
-        //
+        $inputs = $request->validated();
+
+        if ($request->hasFile('image')) {
+            if (!empty($post->image)) {
+                $imageService->deleteDirectoryAndFiles($post->image['directory']);
+            }
+            $imageService->setExclusiveDirectory('images' . DIRECTORY_SEPARATOR . 'posts');
+            $result = $imageService->createIndexAndSave($request->file('image'));
+            if ($result === false) {
+                return back();
+            }
+            $inputs['image'] = $result;
+        }
+
+        $result = $post->update($inputs);
+
+        return Response::withStatus(200)->withAppends([new PostResource($post->fresh())])->send();
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(post $post, ImageService $imageService)
     {
-        //
+        if (!empty($post->image)) {
+            $imageService->deleteDirectoryAndFiles($post->image['directory']);
+        }
+
+        $result = $post->delete();
+
+        return Response::withStatus(200)->withData([])->send();
+    }
+
+    public function changeStatus(Post $post)
+    {
+        $result = $post->update([
+            'status' => $post->status === 1 ? 0 : 1
+        ]);
+        return Response::withStatus(200)->withAppends([new PostResource($post->fresh())])->send();
+
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\CategoryController;
+use App\Http\Controllers\Api\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -10,3 +11,4 @@ Route::get('/user', function (Request $request) {
 
 
 Route::apiResource('category',CategoryController::class);
+Route::apiResource('post',PostController::class);
